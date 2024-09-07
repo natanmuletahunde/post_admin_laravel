e3eef644a3549cb76bfb0fc60047a630b4467de7
pivnati <natanmuleta911@gmail.com>
2024-09-06 20:27:40 -0700

    create the index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Posts</h1>
+    <a href="{{ route('posts.create') }}" class="btn btn-primary">Add Post</a>
+    <table class="table mt-3">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Image</th>
+                <th>Description</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($posts as $post)
+                <tr>
+                    <td>{{ $post->name }}</td>
+                    <td><img src="{{ asset('storage/' . $post->image) }}" width="100" /></td>
+                    <td>{{ $post->description }}</td>
+                    <td>
+                        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-warning">Edit</a>
+                        <form action="{{ route('posts.destroy', $post->id) }}" method="POST" style="display:inline;">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+@endsection
